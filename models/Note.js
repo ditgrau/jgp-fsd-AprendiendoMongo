21d1173d1156070037eb58e8f94e0160fe76c2be
DitGrau <juditgraup@gmail.com>
2023-06-09 16:28:17 +0200

    FIX: modificacion Json res del Note Schema

--- models/Note.js
+++ models/Note.js
@@ -7,6 +7,14 @@ const noteSchema = new Schema({
 })
 
 
+noteSchema.set('toJSON', { //hemos modificado los datos que recibimos del JSON 
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id    
+        delete returnedObject.__v    
+    }
+})
+
 /* ahora modelo. Crear una clase que nos va a permitir 
 crear instancias de este schema "crea un modelo que tenga este schema y este nombre" */
 
